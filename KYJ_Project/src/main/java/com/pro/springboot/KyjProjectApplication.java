5d8b0e1a70db94ebe7b72db37741f932b454c2db
rowin1102 <rowin1102@naver.com>
2025-08-04 16:09:00 +0900

    2025-08-04

---
+++ KYJ_Project/src/main/java/com/pro/springboot/KyjProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pro.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyjProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyjProjectApplication.class, args);
+	}
+
+}
